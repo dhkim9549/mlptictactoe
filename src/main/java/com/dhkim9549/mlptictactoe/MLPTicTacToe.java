4d198650a169189e8ec73d91d595ee9836f010e6
dhkim9549 <dhkim95@hotmail.com>
2017-06-27 17:24:26 +0900

    bbb

--- src/main/java/com/dhkim9549/mlptictactoe/MLPTicTacToe.java
+++ src/main/java/com/dhkim9549/mlptictactoe/MLPTicTacToe.java
@@ -73,7 +73,7 @@ public class MLPTicTacToe {
 
             opponentPool.add(new Policy(model, 0.0));
 
-            evaluate(new Policy(model, 0.1, true), new SupervisedPolicy());
+            evaluate(new Policy(model, 0.0, true), new SupervisedPolicy());
             evaluateModel(model);
 
             if(i % 10 == 0) {
