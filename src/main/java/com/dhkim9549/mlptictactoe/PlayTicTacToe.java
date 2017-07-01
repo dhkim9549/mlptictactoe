989466a3ed89f4f07c13975a0e8588143664f15f
dhkim9549 <dhkim95@hotmail.com>
2017-07-01 15:41:48 +0900

    bbb

---
+++ src/main/java/com/dhkim9549/mlptictactoe/PlayTicTacToe.java
@@ -0,0 +1,49 @@
+package com.dhkim9549.mlptictactoe;
+
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by user on 2017-07-01.
+ */
+public class PlayTicTacToe {
+
+    public static void main(String[] args) throws Exception {
+
+        MultiLayerNetwork model = MLPTicTacToe.readModelFromFile("/down/ttt_model_h2_uSGD_mb16_ss16_5210000.zip");
+
+        while (true) {
+
+            Policy aiPolicy = new Policy(model, 0.0);
+            playGame(new HumanPolicy(), aiPolicy);
+        }
+    }
+
+
+    private static void playGame(Policy policy1, Policy policy2) {
+
+        GameState gs = new GameState();
+
+        while(!gs.isOver()) {
+
+            int a = 0;
+            if(gs.getNextPlayer() == 1) {
+                a = policy1.chooseMove(gs, false);
+            } else {
+                a = policy2.chooseMove(gs, false);
+            }
+
+            gs.playMove(a);
+        }
+
+        System.out.println("gs = " + gs);
+        System.out.println("*************************");
+        System.out.println("Game Over");
+        System.out.println("*************************");
+    }
+}
